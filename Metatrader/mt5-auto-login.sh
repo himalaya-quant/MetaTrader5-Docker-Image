bccdedb369d2b2cac6313ad999281c1e9f29d943
Caius Citiriga <cc@kiboko.it>
2024-12-04 20:35:20 +0100

    debug

--- Metatrader/mt5-auto-login.sh
+++ Metatrader/mt5-auto-login.sh
@@ -8,8 +8,8 @@ echo "###################################################"
 echo "#                   WHOAMI-END                    #"
 echo "###################################################"
 
-# cd /home/abv
-# sudo git clone git@github.com:himalaya-quant/kasmvnc-mt5-controller.git
-# cd kasmvnc-mt5-controller
-# npm i
-# npm start
+cd /Metatrader
+sudo git clone git@github.com:himalaya-quant/kasmvnc-mt5-controller.git
+cd kasmvnc-mt5-controller
+npm i
+npm start
