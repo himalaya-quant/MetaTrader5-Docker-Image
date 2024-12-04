95915b4f7bb443c8e08bfe8c5dd58dd57164d52a
Caius Citiriga <cc@kiboko.it>
2024-12-04 22:43:56 +0100

    passing server via args to node script

--- Metatrader/mt5-auto-login.sh
+++ Metatrader/mt5-auto-login.sh
@@ -4,4 +4,5 @@ cd /Metatrader
 sudo git clone https://github.com/himalaya-quant/kasmvnc-mt5-controller.git
 cd kasmvnc-mt5-controller
 sudo npm i
-sudo npm start
+sudo npm run build
+sudo node dist/main.js mt5_server=${MT5_SERVER}
