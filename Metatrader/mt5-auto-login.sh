ad63ae1d18527195f58ebf1d98c441ebe99eae4c
Caius Citiriga <cc@kiboko.it>
2024-12-05 10:18:07 +0100

    passing username & password to kasmvnc-mt5-controller

--- Metatrader/mt5-auto-login.sh
+++ Metatrader/mt5-auto-login.sh
@@ -5,4 +5,4 @@ sudo git clone https://github.com/himalaya-quant/kasmvnc-mt5-controller.git
 cd kasmvnc-mt5-controller
 sudo npm i
 sudo npm run build
-sudo node dist/main.js mt5_server=${MT5_SERVER}
+sudo node dist/main.js mt5_server=${MT5_SERVER} kasmvnc_username=${CUSTOM_USER} kasmvnc_password=${PASSWORD}
