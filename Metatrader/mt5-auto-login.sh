277cc0808cef23fb73ebd7c68ba5854f9b1248fc
Caius Citiriga <cc@kiboko.it>
2024-12-06 05:02:26 +0100

    feat: passing vars to vnc-mt5-controller

--- Metatrader/mt5-auto-login.sh
+++ Metatrader/mt5-auto-login.sh
@@ -5,4 +5,4 @@ sudo git clone https://github.com/himalaya-quant/kasmvnc-mt5-controller.git
 cd kasmvnc-mt5-controller
 sudo npm i
 sudo npm run build
-sudo node dist/main.js mt5_server=${MT5_SERVER} kasmvnc_username=${CUSTOM_USER} kasmvnc_password=${PASSWORD}
+sudo node dist/main.js mt5_server=${MT5_SERVER} kasmvnc_username=${CUSTOM_USER} kasmvnc_password=${PASSWORD} hq_instances_manager_url=${HQ_INSTANCES_MANAGER_READY_URL} instance_jwt=${HQ_INSTANCE_JWT}
