e0935cc3857f2e7f62352f17c09af20d1885f7ab
Caius Citiriga <cc@kiboko.it>
2024-12-08 22:36:36 +0100

    fixed interpolation of mt5_init script

--- Metatrader/init-config.sh
+++ Metatrader/init-config.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-echo "##########################################"
-echo "# MT5 PWD                                #"
-echo "##########################################"
-echo $MT5_PASSWORD
-echo "##########################################"
-
 # Create the initial configuration files
 cp /Metatrader/mt5_initial_config.ini /config/.wine/drive_c/
 echo "[Common]" >> /config/.wine/drive_c/mt5_initial_config.ini
-echo "Login=${MT5_LOGIN}" >> /config/.wine/drive_c/mt5_initial_config.ini
-echo "Password=${MT5_PASSWORD}" >> /config/.wine/drive_c/mt5_initial_config.ini
-echo "Server=${MT5_SERVER}" >> /config/.wine/drive_c/mt5_initial_config.ini
+echo "Login="$MT5_LOGIN >> /config/.wine/drive_c/mt5_initial_config.ini
+echo "Password="$MT5_PASSWORD >> /config/.wine/drive_c/mt5_initial_config.ini
+echo "Server="$MT5_SERVER >> /config/.wine/drive_c/mt5_initial_config.ini
